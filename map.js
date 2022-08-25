6cbcf8768f6e999245862d44228ad698634466d0
faazizpro <fahadafs11@gmail.com>
2022-08-25 16:16:37 +0600

    Array map to do one line loop magic

---
+++ map.js
@@ -0,0 +1,41 @@
+const numbers = [2, 8, 4, 6, 3];
+const output = [];
+for (const number of numbers) {
+    const doubled = number * 2;
+    output.push(doubled);
+}
+
+// console.log(output);
+
+function getDouble(numbers) {
+    const output = [];
+    for (const number of numbers) {
+        const doubled = doubleIt(number);
+        output.push(doubled);
+    }
+
+    return output;
+}
+
+// function doubleItOld (number){
+//     return number * 2;
+// }
+
+
+const doubleIt = num => num * 2;
+const makeDouble = numbers.map(doubleIt);
+const makeDoubleDirect = numbers.map(num => num * 2);
+
+
+
+/*
+Purpose:
+1. Get an Array
+2. For every elements of the array do something
+3. Store the result in an array
+4. return the result array
+*/
+
+const result = getDouble(numbers);
+console.log(result);
+console.log(makeDouble);
