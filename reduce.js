eb63a50a1a1e380d4744d0466308fcb4168c28cf
faazizpro <fahadafs11@gmail.com>
2022-08-25 22:44:18 +0600

    Learned reduce js

---
+++ reduce.js
@@ -0,0 +1,11 @@
+// map, foreach, filter, find, reduce
+const numbers = [1, 2, 3, 4, 5];
+
+//.reduce (accumulator function, initial value)
+//accumulator function use two parameters
+// const total = numbers.reduce( (previous, current) => previous + current , 0);
+const total = numbers.reduce( (previous, current) => {
+    console.log(previous, current);
+    return previous + current
+} , 0);
+console.log(total);
