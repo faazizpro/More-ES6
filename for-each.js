7c804aab72300e5b7401dd48b898a7edfcda0b85
faazizpro <fahadafs11@gmail.com>
2022-08-25 17:50:50 +0600

    map-string-array-array-of-objects-map-foreach done

---
+++ for-each.js
@@ -0,0 +1,13 @@
+const friends = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'Tom  Jerry'];
+
+friends.forEach(friend => console.log(friend));
+
+
+const products = [
+    {id:1, name: 'mobile', price: 34000},
+    {id:2, name: 'laptop', price: 234000},
+    {id:3, name: 'watch', price: 64000},
+    {id:4, name: 'tablet', price: 24000},
+]
+
+products.forEach(product => console.log(product));
